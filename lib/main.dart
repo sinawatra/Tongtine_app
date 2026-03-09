5f69d5fba87faaffef3a752902cccd931f8dc001
JPSE <watrasinarith@gmail.com>
2026-03-09 22:51:12 +0700

    refactor: off debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
@@ -11,6 +10,7 @@ void main() {
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
       showPerformanceOverlay: false,
+      debugShowCheckedModeBanner: false,
       fallbackLocale: Locale('en', 'US'),
       //  translations: AppTranslations(),R
       // builder: (BuildContext context, Widget? child) {
